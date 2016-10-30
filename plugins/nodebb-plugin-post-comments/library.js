bc42544cb91722b9530f748673891c7b89363bb7
qmm-g-Excellent <qmmahbc@163.com>
2016-10-30 16:02:01 +0800

    can send request from front to end

--- plugins/nodebb-plugin-post-comments/library.js
+++ plugins/nodebb-plugin-post-comments/library.js
@@ -1,26 +1,43 @@
-(function(module){
-	var comment = {};
-	comment.addScripts = function(scripts,callback){
+(function (module) {
+
+	'use strict';
+	var comments = {};
+	comments.addScripts = function (scripts, callback) {
 
 
 		console.log("我是一个新插件");
 
 
-
-
-
 		scripts.push('plugins/nodebb-plugin-post-comments/lib/main.js');
-
 		callback(null, scripts);
 
-	}
+	};
+
+	comments.init = function (params, callback) {
+
+		console.log("我是后台取数据的");
 
 
+		var app = params.router,
+			middleware = params.middleware,
+			controllers = params.controllers;
 
-	module.exports = comment;
+		app.post('/posts/getComments', comments.replyPost);
+		callback();
+
+	};
+
+	comments.replyPost = function (req, res, callback) {
+
+		var content = req.body.content;
+		console.log(content);
+		res.json({result:"发射数据来后台"});
+
+
+	};
+
+	module.exports = comments;
 }(module));
-
-
 
 
 // $.ajax({
--- plugins/nodebb-plugin-post-comments/public/lib/main.js
+++ plugins/nodebb-plugin-post-comments/public/lib/main.js
@@ -1,17 +1,39 @@
 "use strict";
 
-(function(){
+(function () {
 
-	// jQuery('document').ready(function() {
-		$(window).on('action:script.load', function(ev, data) {
+	jQuery('document').ready(function () {
+		$(window).on('action:script.load', function (ev, data) {
 
+			$(".post-tools").append("<a>展开</a>");
 			console.log("新插件里面的");
-
 
 		});
 
-	// });
+		$(window).on('action:script.load',getComments);
 
+	});
+
+	function getComments() {
+
+		console.log("传数据");
+
+		$.ajax({
+			type: "POST",
+			url: nodeBBURL + '/posts/getComments',
+			data:{content:"传数据nodebb"},
+			dataType: "json/application",
+			success:function(result){
+				console.log(result.result);
+
+				console.log("来自后台返回的数据，去哈哈");
+			},
+			error:function(){
+				console.log('error');
+			}
+
+		})
+	}
 
 
 }());
