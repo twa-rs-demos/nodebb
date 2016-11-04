c55bf2679660493bb5c25992b49607617e876de7
qmm-g-Excellent <qmmahbc@163.com>
2016-11-04 21:57:33 +0800

    can send request and triggle request in moment of into a topic

---
+++ isnertData-mongo/db.js
@@ -0,0 +1,87 @@
+// var url = "mongodb://localhost:27017/nodebb";
+// var MongoClient = require("mongodb").MongoClient;
+//
+// MongoClient.connect(url, function (err, db) {
+// 	// assert.equal(null, err);
+// 	console.log("connected correctly to server!");
+// 	insertDocuments(db, function (doc) {
+//
+// 		console.log(doc);
+// 		return doc;
+//
+// 	});
+// });
+//
+//
+// var insertDocuments = function (db, callback) {
+// 	var collection = db.collection("objects");
+// 	collection.update({"pid": 103}, {
+// 		$push: {
+// 			"comments": {
+// 				"pid": 103,
+// 				"comid": 1,
+// 				"uid": 1,
+// 				"comContents": "我不管，我最美，啊哈哈哈哈"
+// 			}
+// 		}
+// 	}, function (err, result) {
+//
+// 		console.log("Inserted 3 documents into teh documents collection!");
+// 		console.log(result);
+// 		callback(result);
+// 	});
+// };
+
+//
+//  function insertData(){
+//
+// 	db.objects.update({"pid": 103}, { $push: {
+// 		   "comments": {
+// 				"pid": 103,
+// 				"comid": 1,
+// 				"uid": 1,
+// 				"comContents": "我不管，我最美，啊哈哈哈哈"
+// 			}
+// 		}
+// 	});
+// }
+//
+// insertData();
+
+var url = "mongodb://localhost:27017/nodebb";
+var db = connect(url);
+
+db.objects.update({"pid": 103}, {
+	$push: {
+		"comments": {
+			"pid": 103,
+			"comid": 1,
+			"uid": 1,
+			"comContents": "我不管，我最美，啊哈哈哈哈"
+		}
+	}
+});
+
+
+db.objects.update({"pid": 103}, {
+	$unset: {
+		"comments": [
+			{
+				"pid": 103,
+				"comid": 1,
+				"uid": 1,
+				"comContents": "我不管，我最美，啊哈哈哈哈"
+			},
+			{
+				"pid": 103,
+				"comid": 1,
+				"uid": 1,
+				"comContents": "我不管，我最美，啊哈哈哈哈"
+			}
+		]
+	}
+});
+
+
+// db.objects.deleteOne({"comid": 1}, function (err,callback) {
+// });
